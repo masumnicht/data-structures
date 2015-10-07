4f1f2d406028eb100e16f56629a33e9169878509
Matt Sumnicht <masumnicht@gmail.com>
2015-10-07 12:25:14 -0700

    Completed enqueue and deque by adding loop to search from back to front causing queue to progress according to length;

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,18 +7,23 @@ var Queue = function() {
   // Implement the methods below
   var length = 0;
   someInstance.enqueue = function(value) {
+    for(var i=length;i>=0;i--){
+      storage[i] = storage[i-1]===undefined ? value : storage[i-1]
+    }
     length++;
   };
 
   someInstance.dequeue = function() {
+    var result = storage[length-1]
     if(length>0){
       length--;
     }
+    delete storage[length];
+    return result;
   };
 
   someInstance.size = function() {
     return length;
   };
-
   return someInstance;
 };
