253bc4cae839311665ccad70e2b3c741259853e4
masumnicht <masumnicht@gmail.com>
2015-10-08 10:49:58 -0700

    Completed tests for linked lists;

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var current = Node(value);
+    
+    if(!list.head){
+      list.head = current
+    } else {
+      list.tail.next = current;
+    }
+    list.tail = current;
+
   };
 
   list.removeHead = function() {
+    var tempHead = list.head;
+    list.head = list.head.next;
+    return tempHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while(currentNode){
+      if(currentNode.value===target){
+        return true;
+      }
+      currentNode = currentNode.next;
+      
+    }
+    return false;
   };
 
   return list;
