6c8365620ee9f75cec550bb2692aa4877939ca3a
masumnicht <masumnicht@gmail.com>
2015-10-08 21:07:05 -0700

    Fixed tree contains function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -20,24 +20,38 @@ treeMethods.addChild = function(value) {
 
 treeMethods.contains = function(target) {
   //var current = startoflist
-  var current = this.children;
-    for(var i = 0 ; i < current.length; i++) {
-      //while currentchildren is not null
-      while(current[i]) {
-        if(current[i].value === target) {
-          return true;
-        } else {
-          console.log(current[i]);
-          current[i] = current[i].children;
-          console.log(current[i])
-        }
-      }
+  var current = this;
+var check = false;
+
+var inner = function (obj) {
+ 
+ for(var i = 0; i < obj.children.length; i++) {
+  if(obj.children[i].value === target ) {
+    check = true;
+    }
+    if(obj.children[i].children.length > 0) {
+      inner(obj.children[i])
+    }
+ }
+ // }    for(var i = 0 ; i < current.length; i++) {
+ //      //while currentchildren is not null
+ //      while(current[i]) {
+ //        if(current[i].value === target) {
+ //          return true;
+ //        } else {
+ //          console.log(current[i]);
+ //          current[i] = current[i].children;
+ //          console.log(current[i])
+ //        }
+ //      }
          //check is current child contains value
            //if it does return true
            //if not reassign current children to be current child
 //return false
   }
-  return false;
+  inner(current);
+  return check;
+
 };
 
 
