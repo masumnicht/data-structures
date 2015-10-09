46ade6f879c29d5deadf558358f56e706e0e887e
masumnicht <masumnicht@gmail.com>
2015-10-08 21:57:18 -0700

    Got insert method working on BinarySearchTrees

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,38 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(searchTreemethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
+var searchTreemethods = {};
 
+searchTreemethods.insert = function (value) {
+    var inserted = false;
+    var current = this;
+    while(!inserted) {
+    if(value < current.value ) {
+        if(current.left === null ) { 
+            current.left = BinarySearchTree(value);
+            inserted = true;
+    }  else{ current = current.left }
+} else if (value > current.value) {
+     if(current.right === null ) { 
+            current.right = BinarySearchTree(value);
+            inserted = true;
+    }
+    else{ current = current.right }
+}
+}
+}
 
+searchTreemethods.contains = function (value) {
+  current = this;
+}
+
+searchTreemethods.depthFirstLog = function () {
+  
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
