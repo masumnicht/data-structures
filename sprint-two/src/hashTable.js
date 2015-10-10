0663f8cff4d0019f5605de5da5a21557e66a181d
masumnicht <masumnicht@gmail.com>
2015-10-10 16:05:40 -0700

    Fixed rersize function on HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,40 +1,74 @@
 
 
 var HashTable = function() {
+  this._size = 0;
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var tupel = [k,v];
-  if(!this._storage.get(index)){
-    this._storage.set(index, []);
+  var bucket = this._storage.get(index) || [];
+
+  for(var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if(tuple[0] === k) {
+      tuple[1] = v;
+      return;
+    }
   }
-  this._storage.get(index).push(tupel);
+  bucket.push([k,v])
+  this._storage.set(index, bucket);
+  this._size++;
+  if(this._size > this._limit * 0.75){
+    this._resize(this._limit * 2)
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
-  var result;
+  var bucket = this._storage.get(index) || [];
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k){
-      result = bucket[i][1];
+      return bucket[i][1];
     }
   }
-  return result === undefined ? null : result;
+  return null;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k){
-      bucket[i] = [null, null];
+    var tuple = bucket[i];
+    if (tuple[0] === k){
+      bucket.splice(i,1);
+      this._size--;
+
+      if(this._size < this._limit * 0.25){
+        this._resize(Math.floor(this._limit / 2))
+      }
+      return tuple[1];
     }
   }
+  
+
 };
+
+HashTable.prototype._resize = function(newLimit) {
+  var oldStorage = this._storage;
+  this._limit = newLimit;
+  this._storage = LimitedArray(newLimit); 
+  this._size = 0;
+  oldStorage.each(function (bucket) {
+    if (!bucket) {return;}
+      for(var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+        this.insert(tuple[0], tuple[1]);
+      }
+  }.bind(this))
+}
 
 
 
